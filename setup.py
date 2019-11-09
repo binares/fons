d17623f1d2b79aefbc902a08cbe1cf31143bb0b3
binares <binares@protonmail.com>
2019-11-10 01:56:01 +0200

    Add more descriptions

--- setup.py
+++ setup.py
@@ -1,10 +1,20 @@
+import pathlib
 from setuptools import setup, find_packages
+
+HERE = pathlib.Path(__file__).parent
+README = (HERE / "README.md").read_text()
 
 setup(
    name='fons',
    version='0.1.0',
-   description='A broad range of python tools. Some uses: round datetimes, verify input data, execute functions by schedule, parse argv',
+   description='A broad range of python tools. Some uses: round datetimes, '\
+               'verify input data, execute functions by schedule, parse argv',
+   long_description=README,
+   long_description_content_type='text/markdown',
+   url='https://github.com/binares/fons',
    author='binares',
+   author_email='binares@protonmail.com',
+   license='MIT',
    packages=find_packages(),
    exclude=['test'],
    python_requires='>=3.5',
